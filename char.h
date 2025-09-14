d968b56bb852b9fcd32957f90e319d38ffea519a
downadow <downadow998@gmail.com>
2025-09-14 15:39:32 +0300

    update

---
+++ char.h
@@ -0,0 +1,27 @@
+/*  iiixmish2-x86 -- the iiixmish2 emulator
+    See COPYING file for license text.
+    Copyright (C) 2025  Menshikov Sviatoslav Sergeevich
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, see <https://www.gnu.org/licenses/>.
+ */
+
+#ifndef __keys_h
+#define __keys_h   1
+
+#include "types.h"
+
+uint8 char_decode(void);
+
+#endif
+
